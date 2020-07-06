7d232c445fb8a37d39993c883ea7f48b08843edd
Yassin Hussein <4assin@gmail.com>
2020-07-06 12:55:43 +0200

    add suggested books

--- src/Components/searchruslut.js
+++ src/Components/searchruslut.js
@@ -46,7 +46,7 @@ const SearchRusults = () => {
 					const res = await fetch(
 						"https://www.googleapis.com/books/v1/volumes?q=" +
 							transccript +
-							"&key=AIzaSyDPEB6OF1CbUKLIsJqI-2deQJcBZJ1yuDE"
+							""
 					);
 					const json = await res.json();
 					console.log("response ===", json);
@@ -73,7 +73,7 @@ const SearchRusults = () => {
 				const res = await fetch(
 					"https://www.googleapis.com/books/v1/volumes?q=" +
 						searchQ +
-						"&key=AIzaSyDPEB6OF1CbUKLIsJqI-2deQJcBZJ1yuDE"
+						""
 				);
 				const json = await res.json();
 				console.log("response ===", json);
@@ -115,7 +115,6 @@ const SearchRusults = () => {
 						<i  className="search__icon">
 						<Link className="back__link" to="/">
 							<svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="long-arrow-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-long-arrow-right fa-w-14"><path fill="currentColor" d="M311.03 131.515l-7.071 7.07c-4.686 4.686-4.686 12.284 0 16.971L387.887 239H12c-6.627 0-12 5.373-12 12v10c0 6.627 5.373 12 12 12h375.887l-83.928 83.444c-4.686 4.686-4.686 12.284 0 16.971l7.071 7.07c4.686 4.686 12.284 4.686 16.97 0l116.485-116c4.686-4.686 4.686-12.284 0-16.971L328 131.515c-4.686-4.687-12.284-4.687-16.97 0z" class=""></path></svg>
-
 						</Link>
 						</i>
 						<input
